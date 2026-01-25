752ff6838a477bd34f8b1d0c1c52905c73a18b54
Mohammad Mahamid <mzkariyah@gmail.com>
2026-01-25 20:28:01 +0100

    docs: add JavaDoc and usage examples to model classes

--- model/src/main/java/com/mohamad4444/github/CategoryDTO.java
+++ model/src/main/java/com/mohamad4444/github/CategoryDTO.java
@@ -1,14 +1,57 @@
 package com.mohamad4444.github;
+
 import com.mohamad4444.github.DbEntity;
 import com.mohamad4444.github.DbField;
 
 import lombok.*;
 
+/**
+ * Example DTO demonstrating the use of {@link DbEntity} and {@link DbField}
+ * annotations.
+ * 
+ * <p>
+ * This class represents a category entity from the "ev_category" database
+ * table.
+ * When compiled, the annotation processor generates a {@code CategoryDTODB}
+ * class
+ * that extends {@code AbstractDB<CategoryDTO>} with automatic ResultSet
+ * mapping.
+ * 
+ * <p>
+ * <b>Generated Code:</b><br>
+ * The processor generates {@code CategoryDTODB.java} in
+ * {@code target/generated-sources/annotations/} with a {@code buildDTO} method
+ * that:
+ * 
+ * <pre>{@code
+ * protected CategoryDTO buildDTO(ResultSet rs) throws SQLException {
+ *     return new CategoryDTO(
+ *             rs.getInt("ekid"),
+ *             rs.getString("category_name"),
+ *             rs.getString("color"),
+ *             rs.getInt("evid"));
+ * }
+ * }</pre>
+ * 
+ * <p>
+ * <b>Usage:</b><br>
+ * The generated class can be used in your data access layer:
+ * 
+ * <pre>{@code
+ * CategoryDTODB categoryDb = new CategoryDTODB();
+ * List<CategoryDTO> categories = categoryDb.executeQuery(
+ *         connection,
+ *         "SELECT * FROM ev_category");
+ * }</pre>
+ * 
+ * @see DbEntity
+ * @see DbField
+ */
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@DbEntity(tableName = "ev_category") 
+@DbEntity(tableName = "ev_category")
 public class CategoryDTO {
 
     @DbField(columnName = "ekid", isPrimaryKey = true)
--- model/src/main/java/com/mohamad4444/github/Employee.java
+++ model/src/main/java/com/mohamad4444/github/Employee.java
@@ -3,6 +3,55 @@ package com.mohamad4444.github;
 
 import com.mohamad4444.github.Builder;
 
+/**
+ * Example class demonstrating the {@link Builder} annotation.
+ * 
+ * <p>
+ * <b>⚠️ NOTE:</b> The {@code @Builder} annotation is not yet implemented.
+ * This class serves as a placeholder example for future builder pattern
+ * code generation functionality.
+ * 
+ * <p>
+ * When implemented, the processor would generate an {@code EmployeeBuilder}
+ * class with fluent setter methods and a {@code build()} method.
+ * 
+ * <p>
+ * <b>Planned Generated Code:</b>
+ * 
+ * <pre>{@code
+ * public class EmployeeBuilder {
+ *     private String name;
+ *     private int age;
+ * 
+ *     public EmployeeBuilder name(String value) {
+ *         name = value;
+ *         return this;
+ *     }
+ * 
+ *     public EmployeeBuilder age(int value) {
+ *         age = value;
+ *         return this;
+ *     }
+ * 
+ *     public Employee build() {
+ *         return new Employee(name, age);
+ *     }
+ * }
+ * }</pre>
+ * 
+ * <p>
+ * <b>Planned Usage:</b>
+ * 
+ * <pre>{@code
+ * Employee emp = new EmployeeBuilder()
+ *         .name("John Doe")
+ *         .age(30)
+ *         .build();
+ * }</pre>
+ * 
+ * @see Builder
+ */
+@SuppressWarnings("deprecation") // Builder is not yet implemented
 @Builder
 public class Employee {
 
